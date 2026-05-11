0e583ffedad7a670d81941ae8573aec3020f1bb9
D45-csm <ownercreater@gmail.com>
2026-05-11 12:39:25 -0500

    python

---
+++ listas.py
@@ -0,0 +1,35 @@
+# LISTAS
+
+#estructura de listas 
+
+#indice: 0           1
+listas = ["objeto_1", "objeto_2", "objeto_3"]
+print (type(listas)) #<class "list">
+
+#CREAR UNA LISTA
+
+#indice           0         1       2       3        4         5         6       
+#indice           -6        -5      -4     -3        -2         -1        0
+estudiantes = ["yisel", "laura", "luis", "jhon", "stiven", "ronalt", "andres"]
+print(estudiantes)
+
+#ACCEDER A UN ELEMENTO DE LA LISTA
+print(estudiantes[5]) #ronalt
+
+#MODIFICAR UN ELEMENTO DE LA LISTA
+estudiantes[3]= "miguel"
+print(estudiantes)
+
+#consultar rangos de elementos de la lista 
+print(estudiantes[0:5]) # ["yisel", laura]
+print(estudiantes[:3]) # ["yisel", "laura", "luis"]
+print(estudiantes[3:3]) #[]
+print(estudiantes[0:2:4])
+
+aprendices_ficha_3321349 = ["yisel", "laura", "luis", "jhon", "stiven", "ronalt", "andres"]
+aprendices_ficha_3321349 = ["miguel", "laura", "mariam", "sergio", "yosel", "ronalt", "pedro"]
+
+#concatenar listas
+
+
+
